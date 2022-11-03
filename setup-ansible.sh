76cb249916f7b8562af6e172b2fcab6dc21f0a56
operator873 <operator873@gmail.com>
2022-11-02 20:24:34 -0500

    Add beginnings of ansible setup automation

---
+++ setup-ansible.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script automates creating an ansible user, adding the pubkey for the user
+# and finally adding an entry to /etc/sudoers.d for ansible
+#
+# Before using, be sure to export your pubkey as a variable call MYANSIBLEKEY like:
+# export MYANSIBLEKEY=$(cat /home/ansible/id_rsa.pub)
+
+if [ $UID -ne 0 ]; then
+  echo "This script must be run as root (sudo)"
+  exit 1
+elif [ $# -eq 0 ]; then
+  echo "Please provide desire ansible username. Example: ./this-script.sh ansible"
+  exit 1
+fi
+
+useradd -m -G wheel ${1}
+if [ -d "/home/${1}" ]; then
+  cd /home/${1}
+  mkdir .ssh
+  chmod 700 .ssh
+  cd .ssh
+  echo ${MYANSIBLEKEY} > authorized_keys
+  cd /home/${1}
+  chown -R ${1}:${1} .ssh
+else
+  echo "Unknown error occurred during home directory creation. Check logs..."
+fi
